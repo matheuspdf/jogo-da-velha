158f33217992cadd4eb0d5f53c0afd581349594a
matheuspdf <96511087+matheuspdf@users.noreply.github.com>
2022-04-07 19:18:24 -0300

    script.js

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ document.querySelectorAll('.item').forEach(item => {
 //Functions
 function itemClick(event) {
   let item = event.target.getAttribute('data-item')
-  if (square[item] === '') {
+  if (playing && square[item] === '') {
     square[item] = player
     renderSquare()
     togglePlayer()
@@ -56,6 +56,8 @@ function renderSquare() {
     let item = document.querySelector(`div[data-item=${i}]`)
     item.innerHTML = square[i]
   }
+
+  checkGame()
 }
 
 function renderInfo() {
@@ -64,9 +66,51 @@ function renderInfo() {
 }
 
 function togglePlayer() {
-  if (player === 'x') {
-    player = 'o'
-  } else {
-    player = 'x'
+  player = player === 'x' ? 'o' : 'x'
+  renderInfo()
+}
+
+function checkGame() {
+  if (checkWinnerFor('x')) {
+    warning = 'O "x" venceu'
+    playing = false
+  } else if (checkWinnerFor('o')) {
+    warning = 'O "o" venceu'
+    playing = false
+  } else if (isFull()) {
+    warning = 'Deu empate'
+    playing = false
   }
+}
+
+function checkWinnerFor(player) {
+  let pos = [
+    'a1,a2,a3',
+    'b1,b2,b3',
+    'c1,c2,c3',
+    'a1,b1,c1',
+    'a2,b2,c2',
+    'a3,b3,c3',
+    'a1,b2,c3',
+    'a3,b2,c1'
+  ]
+
+  for (let w in pos) {
+    let pArray = pos[w].split(',')
+    let hasWon = pArray.every(option => square[option] === player)
+    if (hasWon) {
+      return true
+    }
+  }
+
+  return false
+}
+
+function isFull() {
+  for (let i in square) {
+    if (square[i] === '') {
+      return false
+    }
+  }
+  return true
 }
